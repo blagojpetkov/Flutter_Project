483072a314cb97930a951bd84a798e485955f727
blagojpetkov <85490029+blagojpetkov@users.noreply.github.com>
2023-09-01 04:52:25 +0200

    Updated versions of Dart SDK, http, add weather.

--- lib/http_service.dart
+++ lib/http_service.dart
@@ -8,44 +8,48 @@ class HttpService {
   HttpService({required this.baseUrl});
 
   void fetchToken() async {
-    final response = await http.post(
-      Uri.parse('$baseUrl/rest-auth/guests?aid=3136'),
-      headers: {"Content-Type": "application/json"},
-    );
-
-    if (response.statusCode == 200 || response.statusCode == 201) {
-      token = json.decode(response.body);
-      if (token is String) {
-        print("Token is set in http_service to: " + (this.token ?? " null"));
-      } else {
-        throw Exception('Failed to fetch token');
-      }
-    }
-  }
-
-    Future<dynamic> get(String endpoint) async {
-      final response = await http.get(Uri.parse('$baseUrl/$endpoint'));
-
-      if (response.statusCode == 200) {
-        return json.decode(response.body);
-      } else {
-        throw Exception('Failed to load data');
-      }
-    }
-
-    Future<dynamic> post(String endpoint, [dynamic data]) async {
+    try {
       final response = await http.post(
-        Uri.parse('$baseUrl/$endpoint'),
+        Uri.parse('$baseUrl/rest-auth/guests?aid=3136'),
         headers: {"Content-Type": "application/json"},
-        body: json.encode(data),
       );
 
       if (response.statusCode == 200 || response.statusCode == 201) {
-        return json.decode(response.body);
-      } else {
-        throw Exception('Failed to post data');
+        token = json.decode(response.body);
+        if (token is String) {
+          print("Token is set in http_service to: " + (this.token ?? " null"));
+        } else {
+          throw Exception('Failed to fetch token');
+        }
       }
+    } catch (error) {
+      print("Network error: $error");
     }
+  }
 
-    // You can also add methods for PUT, DELETE, etc.
+  Future<dynamic> get(String endpoint) async {
+    final response = await http.get(Uri.parse('$baseUrl/$endpoint'));
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  Future<dynamic> post(String endpoint, [dynamic data]) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/$endpoint'),
+      headers: {"Content-Type": "application/json"},
+      body: json.encode(data),
+    );
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to post data');
+    }
+  }
+
+  // You can also add methods for PUT, DELETE, etc.
 }
